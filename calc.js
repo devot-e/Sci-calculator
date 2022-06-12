271ea0845bbc66ae8e904d3001e896f7cb75b922
raj-coder-2019 <r23711268@gmail.com>
2022-06-12 11:19:40 +0530

    new file:   calc.css 	new file:   calc.js 	new file:   calculator.html 	deleted:    main(6jun9pm).html 	deleted:    main(6june).html 	deleted:    main(7june).html 	deleted:    main(old).html 	deleted:    main.css 	deleted:    main.html 	deleted:    main.js 	deleted:    normal_calculator.html

---
+++ calc.js
@@ -0,0 +1,63 @@
+var Exp="";
+var ans=0;
+function errorType(erro){
+    var a='';
+    for(var i=0;i<erro.length;i++){
+        if(erro[i]!=':')a+=erro[i];
+        else break;
+    }
+    return a;
+}
+function Eval(){
+    try {
+        if(Exp==''){
+            document.getElementById("in").innerText=Exp;
+            document.getElementById("out").innerText=ans;    
+        }
+        else{
+            var x;
+            with(Math){
+                x=eval(Exp);
+            }
+            ans=x;
+            document.getElementById("out").innerText=ans;
+        }
+    }
+    catch (error) {
+        ans=0;
+        document.getElementById("out").innerText=error.name;
+        alert(error);
+    }
+}
+function ln(x){
+    return Math.log(x);
+}
+function sq(x){
+    return Math.pow(x,2);
+}
+function inv(x){
+    return Math.pow(x,-1);
+}
+function Ans(){
+    Exp+=ans;
+    
+}
+function numskey(a){
+    Exp+=a;
+    document.getElementById("in").innerText=Exp;
+}
+
+function Clear(){
+    Exp="";
+    document.getElementById("in").innerText=Exp;
+}
+function ClearA(){
+    Exp="";
+    document.getElementById("in").innerText=Exp;
+    document.getElementById("out").innerText=Exp;
+}
+function backspace(){
+    Exp=Exp.slice(0,Exp.length-1);
+    document.getElementById("in").innerText=Exp;
+
+}
